b649180a3b4dd49c994bb5952599ddd4a42e6633
pedroblome <joaopedroblome78@gmail.com>
2021-12-28 20:39:21 -0300

    finishing training

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import VueTheMask from "vue-the-mask";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueTheMask);
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -22,8 +22,8 @@
             label="CEP"
             @blur="getCEP"
             v-model="cep"
-            type="number"
             hide-spin-buttons
+            v-mask="['#####-###']"
           >
           </v-text-field>
         </v-col>
@@ -82,7 +82,6 @@ export default {
     items: ["Item 1", "Item 2", "Item 3", "Item 4"],
     checkbox: false,
   }),
-
   methods: {
     validate() {
       this.$refs.form.validate();
@@ -94,10 +93,11 @@ export default {
       this.$refs.form.resetValidation();
     },
     async getCEP() {
-      if (this.cep.length === 8) {
+      const cepFormatado = this.cep.replace("-", "");
+      if (cepFormatado.length === 8) {
         try {
           const response = await fetch(
-            `https://viacep.com.br/ws/${this.cep}/json/`
+            `https://viacep.com.br/ws/${cepFormatado}/json/`
           );
           const json = await response.json();
           this.cidade = json.localidade;
