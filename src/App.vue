ea236369beab61b081a2b0f903088766fedec749
pedroblome <joaopedroblome78@gmail.com>
2021-12-28 14:07:35 -0300

    login & about routes

--- src/App.vue
+++ src/App.vue
@@ -16,29 +16,7 @@
     </v-app-bar>
     <!-- Login -->
     <v-main>
-      <v-card width="500" class="mx-auto mt-5">
-        <v-card-title><h1 class="display-1">Login</h1></v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field
-              label="Username"
-              prepend-icon="mdi-account-circle"
-            ></v-text-field>
-            <v-text-field
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="exibir ? 'mdi-eye-off' : 'mdi-eye'"
-              :type="exibir ? 'text' : 'password'"
-              @click:append="showHide"
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view> </router-view>
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
@@ -65,13 +43,8 @@ export default {
   name: "App",
   components: {},
   data: () => ({
-    exibir: false,
     links: ["Home", "Login", "API"],
   }),
-  methods: {
-    showHide() {
-      this.exibir = !this.exibir;
-    },
-  },
+  methods: {},
 };
 </script>
