118d807981440b22699ed4c811a0602cd456a362
pedroblome <joaopedroblome78@gmail.com>
2021-12-28 15:33:26 -0300

    adding cep

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-title>Vue + Vuetify</v-app-bar-title>
+      <v-app-bar-title>Trainee</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-btn
-        v-for="link in links"
-        :key="link"
+        v-for="(link, i) in links"
+        :key="i"
         color="white"
         text
         rounded
         class="my-2"
+        :to="link.path"
       >
-        {{ link }}
+        {{ link.label }}
       </v-btn>
     </v-app-bar>
-    <!-- Login -->
     <v-main>
-      <router-view> </router-view>
+      <router-view></router-view>
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
-          v-for="link in links"
-          :key="link"
+          v-for="(link, i) in links"
+          :key="i"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.path"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
@@ -43,7 +44,20 @@ export default {
   name: "App",
   components: {},
   data: () => ({
-    links: ["Home", "Login", "API"],
+    links: [
+      {
+        label: "Home",
+        path: "/",
+      },
+      {
+        label: "Login",
+        path: "/login",
+      },
+      {
+        label: "About",
+        path: "/about",
+      },
+    ],
   }),
   methods: {},
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,7 +17,7 @@
       </v-form>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="success">Register</v-btn>
+      <v-btn color="success" to="/register">Register</v-btn>
       <v-spacer></v-spacer>
       <v-btn color="info">Login</v-btn>
     </v-card-actions>
@@ -26,7 +26,6 @@
 
 <script>
 export default {
-  components: {},
   data: () => ({
     exibir: false,
   }),
@@ -37,7 +36,5 @@ export default {
   },
 };
 </script>
- 
 
-<style scoped>
-</style>
+<style scoped></style>
